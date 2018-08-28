ffebe0d4f8818f66f52412f46f29bb6d1ee0eb27
Vanessa Meneghin <vanny2go@gmail.com>
2018-08-28 18:36:37 -0300

    Add files via upload

---
+++ dogyears.js
@@ -0,0 +1,7 @@
+const myAge = 35;
+const firstYears = 2 * 10.5;
+const laterYears = (myAge - 2) * 4;
+const myAgeInDogYears = firstYears + laterYears;
+
+console.log('I am ' + myAgeInDogYears + ' dog years.');
+
